da25241312800e66b201b8b562ab116de7417a68
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-06-29 21:42:23 +0530

    cleaned up

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,39 +1,38 @@
 
-"use client"
 
 import React from 'react'
 
 function Product() {
-  const fetchProduct = async() => {
-    // const url = 'https://raw.githubusercontent.com/christkv/ecommerce/master/preload_data/products.json';
+  // const fetchProduct = async() => {
+  //   // const url = 'https://raw.githubusercontent.com/christkv/ecommerce/master/preload_data/products.json';
 
-    // const response = await fetch(url);
+  //   // const response = await fetch(url);
 
-    // const text = await response.text();
-    // const categoryRegex = /"category"\s*:\s*"([^"]+)"/g;
-    // const matches = text.match(categoryRegex);
+  //   // const text = await response.text();
+  //   // const categoryRegex = /"category"\s*:\s*"([^"]+)"/g;
+  //   // const matches = text.match(categoryRegex);
 
-    // const categories = matches && matches.map((match) => {
-    //   const capturedGroup = match.match(/"category"\s*:\s*"([^"]+)"/);
-    //   return capturedGroup && capturedGroup[1];
-    // });
+  //   // const categories = matches && matches.map((match) => {
+  //   //   const capturedGroup = match.match(/"category"\s*:\s*"([^"]+)"/);
+  //   //   return capturedGroup && capturedGroup[1];
+  //   // });
 
-    // console.log("categories:", categories);
+  //   // console.log("categories:", categories);
 
-    // console.log(text);
+  //   // console.log(text);
 
-    // fetch('https://fakestoreapi.com/products')
-    //         .then(res=>res.json())
-    //         .then(json=>console.log(json))
+  //   // fetch('https://fakestoreapi.com/products')
+  //   //         .then(res=>res.json())
+  //   //         .then(json=>console.log(json))
 
-    // fetch('https://github.com/iffi96/Shoe-store-data-json/blob/master/data001.json')
-    //         .then(res=>res.json())
-    //         .then(json=>console.log(json))
-    console.log("clicked");
+  //   // fetch('https://github.com/iffi96/Shoe-store-data-json/blob/master/data001.json')
+  //   //         .then(res=>res.json())
+  //   //         .then(json=>console.log(json))
+  //   console.log("clicked");
     
-  }
+  // }
   return (
-    <div onClick={fetchProduct}>Product</div>
+    <div >Product</div>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,6 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 
 
 
