bc37a32866ff9079d2cb481b9578fb6379f40a8d
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-07-10 00:07:31 +0530

    products page

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -4,26 +4,7 @@ import React from 'react'
 
 function page() {
   const fetchProduct = async() => {
-    // const url = 'https://raw.githubusercontent.com/christkv/ecommerce/master/preload_data/products.json';
-
-    // const response = await fetch(url);
-
-    // const text = await response.text();
-    // const categoryRegex = /"category"\s*:\s*"([^"]+)"/g;
-    // const matches = text.match(categoryRegex);
-
-    // const categories = matches && matches.map((match) => {
-    //   const capturedGroup = match.match(/"category"\s*:\s*"([^"]+)"/);
-    //   return capturedGroup && capturedGroup[1];
-    // });
-
-    // console.log("categories:", categories);
-
-    // console.log(text);
-
-    // fetch('https://fakestoreapi.com/products')
-    //         .then(res=>res.json())
-    //         .then(json=>console.log(json))
+    
 
     fetch('https://raw.githubusercontent.com/algolia/datasets/master/ecommerce/bestbuy_seo.json')
             .then(res=>res.json())
@@ -37,7 +18,3 @@ function page() {
 }
 
 export default page
-
-// https://raw.githubusercontent.com/christkv/ecommerce/master/preload_data/products.json
-
-// https://dummyjson.com/docs/products
