b006db48193923c6e052fc50cb51f11a0b8eaf29
Sundaram Kumar Jha <jhasundaram@outlook.com>
2023-07-12 02:43:09 +0530

    added slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -21,32 +21,29 @@ const responsive = {
 };
 
 
-const Slider = () => {
+const Slider = ({children}:{children: React.ReactNode}) => {
     return (
       <div className="w-[80vw] mx-auto mt-5 z-0">
           <Carousel
           swipeable={false}
           draggable={false}
-          showDots={true}
+          // showDots={true}
           responsive={responsive}
           ssr={true} // means to render carousel on server-side.
           infinite={true}
-          autoPlay={true}
-          autoPlaySpeed={1000}
+          // autoPlay={true}
+          // autoPlaySpeed={1000}
           keyBoardControl={true}
           customTransition="all .5"
           transitionDuration={500}
           containerClass="carousel-container"
-          removeArrowOnDeviceType={["tablet", "mobile"]}
+          removeArrowOnDeviceType={["tablet", "mobile", "desktop", "largeDesktop", "pc"]}
           // deviceType={this?.props.deviceType}
-          dotListClass="custom-dot-list-style"
+          // dotListClass="custom-dot-list-style"
           itemClass="carousel-item-padding-40-px"
           className="w-[100%] mx-auto z-0"
           >
-            <div className="bg-slate-400 w-[500px] h-[300px]">Item 1</div>
-            <div className="bg-slate-500 w-[500px] h-[300px]">Item 2</div>
-            <div className="bg-slate-600 w-[500px] h-[300px]">Item 3</div>
-            <div className="bg-slate-700 w-[500px] h-[300px]">Item 4</div>
+            {children}
         </Carousel>
       </div>
   )
