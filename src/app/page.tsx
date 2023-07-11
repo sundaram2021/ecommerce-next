d7bc20e8011f60e1fd6de8eab1ac7982e428caa5
Sundaram Kumar Jha <jhasundaram@outlook.com>
2023-07-12 02:40:07 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-"use client"
-
 import Banner from "@/components/Banner"
 import Prod1 from "@/components/Prod1"
+import Prod2 from "@/components/Prod2"
 
 
 export default function page() {
@@ -9,7 +8,9 @@ export default function page() {
   return (
     <div >
       <Banner />
-      <Prod1 />
+      <Prod1 pd="headphones" />
+      <Prod2 />
+      <Prod1 pd="movies & tv shows" />
     </div>
   )
 }
