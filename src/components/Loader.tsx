703a349b1d9ca1de38964727062bd2e996582047
Sundaram Kumar Jha <jhasundaram@outlook.com>
2023-07-15 01:59:34 +0530

    added loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,16 +1,18 @@
-import React, { FC } from "react";
-import { VscLoading } from 'react-icons/vsc'
+"use client"
+
+import React from "react";
 
 
 
 const Loader = () => {
-    // if(size === null){
-    //     {size} = 0
-    // }
-    return <div className="flex justify-center items-center h-[70vh] w-[100vw] z-50">
-        <button disabled className="flex justify-center items-center">
-            <VscLoading className={`animate-spin text-full height-[100%] duration-50`} />
-        </button>
+    let arr = new Array(12).fill(0);
+    return <div className="ml-16">
+        <div className="items-center grid grid-rows-1 sm:grid-rows-2 md:grid-rows-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2">
+            {arr.map((item, index) => (
+                <div className="w-[250px] h-[250px] bg-[#3c4043] animate-pulse rounded-lg" key={index}></div>
+            ))}
+        </div>
+        
     </div>
 };
 
