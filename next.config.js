3d2e3b9f56b3e8bcd617eee094b847924daef307
Sundaram Kumar Jha <jhasundaram@outlook.com>
2023-07-15 02:04:27 +0530

    modified

--- next.config.js
+++ next.config.js
@@ -18,6 +18,10 @@ const nextConfig = {
             protocol: 'https',
             hostname: "www.google.com",
           },
+          {
+            protocol: 'https',
+            hostname: "images.clerk.dev",
+          }
         ],
       },
 }
