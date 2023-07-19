87f34984af7a8a77a977f81a09ad19a32b20f882
Sundaram Kumar Jha <jhasundaram@outlook.com>
2023-07-19 20:32:57 +0530

    updated

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -2,63 +2,113 @@
 
 import MainProd from '@/components/MainProd';
 import { Separator } from '@/components/ui/separator';
-import React from 'react'
+import React from 'react';
+import { Loader } from 'lucide-react';
+import { Product } from '@/types';
+import { useUser } from "@clerk/nextjs";
+import FilterComponent from '@/components/Filter';
 
-interface Product {
-  name: string;
-  shortDescription: string;
-  bestSellingRank: number;
-  thumbnailImage: string;
-  salePrice: number;
-  manufacturer: string;
-  url: string;
-  type: string;
-  image: string;
-  customerReviewCount: number;
-  shipping: string;
-  salePrice_range: string;
-  objectID: string;
-  categories: string[];
-}
 
-function page({params}: {params: {productId: string}}) {
+function Page({ params }: { params: { productId: string } }) {
   console.log('params', params);
+  const { user } = useUser();
+  console.log('user', user);
+  const userId = user?.id;
+  const email  = user?.primaryEmailAddress?.emailAddress;
   const [product, setProduct] = React.useState<Product>();
   const [productCategory, setProductCategory] = React.useState<string>();
+  const [loading, setLoading] = React.useState<boolean>(false);
 
-
-  const fetchProduct = async() => {
-    const res = await fetch("https://raw.githubusercontent.com/algolia/datasets/master/ecommerce/bestbuy_seo.json");
+  const fetchProduct = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
     const data = await res.json();
     const prod = data.find((product: Product) => product.objectID === params.productId);
     setProduct(prod);
     setProductCategory(prod.categories[0]);
-  }
+  };
 
   React.useEffect(() => {
     fetchProduct();
   }, []);
 
+  
+
   console.log('product', product);
+
+  const handleCart = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+
+    if(!user){
+      alert('Please sign in to add to cart')
+      return;
+    }
+
+    setLoading(true);
+  
+    try {
+      const res = await fetch('/api/cart', {
+        method: 'POST',
+        body: JSON.stringify({
+          name: product?.name,
+          description: product?.shortDescription,
+          productId: product?.objectID,
+          price: product?.salePrice,
+          user: email,
+          userId: userId,
+          username: user?.fullName,
+          img: product?.image,
+          qty: 1,
+        }),
+      })
+      console.log('Cart created:', res.json());
+    } catch (error) {
+      console.error('Error creating cart:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
   
 
   return (
     <div>
-        <div className='w-[90%] mx-auto ml-16 mb-6 mt-10 flex flex-col sm:flex-row  justify-between items-start gap-6'>
-            <div className='sm:w-[50%] w-[85%] rounded-lg shadow-sm shadow-white bg-white'>
-              {product ? <img className='rounded-md  md:h-[300px] sm:h-[300px] h-[200px]' src={product.image} alt="" />: <div className='bg-[#676c76] w-full animate-pulse'></div>}
-            </div>
-            <div className='sm:w-[50%] w-[85%] flex flex-col gap-4'>
-              <h1 className='font-bold'>{product?.name}</h1>
-              <p className='font-thin line-clamp-3' title={product?.shortDescription}>{product?.shortDescription}</p>
-              <p className='font-semibold'>${product?.salePrice}</p>
-              <p className='font-semibold'>Rank: {product?.bestSellingRank}</p>
-            </div>
+      <div className='w-[90%] mx-auto ml-16 mb-6 mt-10 flex flex-col sm:flex-row  justify-between items-start gap-6'>
+        <div className='sm:w-[50%] w-[85%] rounded-lg shadow-sm shadow-white bg-white flex justify-center items-center'>
+          {product ? (
+            <img className='rounded-md  md:h-[300px] sm:h-[300px] h-[200px]' src={product.image} alt='' />
+          ) : (
+            <div className='bg-[#676c76] w-full animate-pulse'></div>
+          )}
         </div>
-        <Separator />
-        <MainProd name={productCategory}  />
+        <div className='sm:w-[50%] w-[85%] flex flex-col gap-4'>
+          <h1 className='font-bold'>{product?.name}</h1>
+          <p className='font-thin line-clamp-3' title={product?.shortDescription}>
+            {product?.shortDescription}
+          </p>
+          <p className='font-semibold'>${product?.salePrice}</p>
+          <p className='font-semibold'>Rank: {product?.bestSellingRank}</p>
+          <div className='flex'>
+            <button
+              className='mr-auto text-black bg-white py-[4px] px-3 rounded-lg border border-black hover:bg-[#424242] duration-300 ease-in-out hover:text-white hover:border-white flex items-center'
+              onClick={handleCart}
+              disabled={loading}
+            >
+              {loading && <Loader className='mr-2 animate-spin' size={20} />} Add to cart
+            </button>
+            <button
+              className='mr-auto text-black bg-white py-[4px] px-3 rounded-lg border border-black hover:bg-[#424242] duration-300 ease-in-out hover:text-white hover:border-white flex items-center'
+              onClick={handleCart}
+            >
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+      <Separator />
+      <FilterComponent />
+      <Separator />
+      <MainProd name={productCategory} pagination={true} />
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
